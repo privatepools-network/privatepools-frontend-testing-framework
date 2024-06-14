a4c5b23e2fdc1bd2948daaa9b5789e17847c5b03
neverm25 <never08mind25@gmail.com>
2024-06-13 23:21:54 -0400

    feat: connect 1inch swap with trades modal

--- src/composables/poolActions/deposit/useZapper.js
+++ src/composables/poolActions/deposit/useZapper.js
@@ -35,7 +35,7 @@ export async function useZapper(pool, srcToken, srcAmount) {
       if (pool.tokens[i].address !== srcToken) {
         const amount = pool.tokens[i].weight * decimalsAmount
 
-        const oneInchTxData = await fetch1InchData(
+        const { data: oneInchTxData } = await fetch1InchData(
           srcToken,
           pool.tokens[i].address,
           amount.toString(),
@@ -80,6 +80,71 @@ export async function useZapper(pool, srcToken, srcAmount) {
   }
 }
 
+export async function useTrades(pool, srcToken, srcAmount, slippage = 1) {
+  try {
+    const provider = await InitializeMetamask()
+    if (!provider) return
+    const signer = provider.getSigner()
+    const config = configService.getNetworkConfig(networkId.value)
+
+    const tokenContract = new ethers.Contract(srcToken, ERC20Abi, signer)
+    const decimals = await tokenContract.decimals()
+    const decimalsAmount = ethers.utils.parseUnits(
+      srcAmount.toString(),
+      decimals,
+    )
+
+    const oneInchDatas = []
+    const oneInchDescs = []
+    const toAmounts = []
+    const fromAmounts = []
+
+    const i1InchRouter = new ethers.utils.Interface(I1InchRouterAbi)
+    for (let i = 0; i < pool.tokens.length; i++) {
+      if (pool.tokens[i].address !== srcToken) {
+        const amount = pool.tokens[i].weight * decimalsAmount
+        fromAmounts.push(pool.tokens[i].weight * srcAmount)
+
+        const oneInchTxData = await fetch1InchData(
+          srcToken,
+          pool.tokens[i].address,
+          amount.toString(),
+          config.addresses.zapper,
+          slippage,
+        )
+        const decodedDatas = i1InchRouter.decodeFunctionData(
+          'swap',
+          oneInchTxData.data,
+        )
+        oneInchDatas.push(decodedDatas[3])
+
+        oneInchDescs.push({
+          srcToken,
+          dstToken: pool.tokens[i].address,
+          srcReceiver: config.addresses.oneInchExecutor,
+          dstReceiver: config.addresses.zapper,
+          amount: amount.toString(),
+          minReturnAmount: 1,
+          flags: 0,
+        })
+        toAmounts.push(
+          ethers.utils.formatUnits(
+            oneInchTxData.toAmount,
+            pool.tokens[i].decimals,
+          ),
+        )
+
+        if (i < pool.tokens.length - 1) await sleep(1000)
+      }
+    }
+
+    return { oneInchDatas, oneInchDescs, fromAmounts, toAmounts }
+  } catch (error) {
+    console.log('Error useTrades - ', error)
+    return error
+  }
+}
+
 const sleep = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
@@ -108,7 +173,7 @@ const fetch1InchData = async (
       params,
     })
 
-    return data.tx.data
+    return { data: data.tx.data, toAmount: data.toAmount }
   } catch (error) {
     throw new Error(error.response.data)
   }
