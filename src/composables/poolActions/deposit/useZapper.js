9bdf941639f87e06da3445fb00273b01fdd330a5
neverm25 <never08mind25@gmail.com>
2024-06-28 07:57:49 -0400

    feat: calculate fee for zap

--- src/composables/poolActions/deposit/useZapper.js
+++ src/composables/poolActions/deposit/useZapper.js
@@ -64,13 +64,20 @@ export async function useTrades(
     if (!provider) return
     const signer = provider.getSigner()
     const config = configService.getNetworkConfig(networkId.value)
+    const zapper = new ethers.Contract(
+      config.addresses.zapper,
+      ZapperAbi,
+      signer,
+    )
+    const fee =
+      (srcAmount * (await zapper.fee())) / (await zapper.FEE_MULTIPLIER())
+    srcAmount -= fee
 
     const tokenContract = new ethers.Contract(srcToken, ERC20Abi, signer)
     const decimals = await tokenContract.decimals()
-    const decimalsAmount =  rawAmount ? srcAmount :ethers.utils.parseUnits(
-      srcAmount.toString(),
-      decimals,
-    )
+    const decimalsAmount = rawAmount
+      ? srcAmount
+      : ethers.utils.parseUnits(srcAmount.toString(), decimals)
 
     const oneInchDatas = []
     const oneInchDescs = []
