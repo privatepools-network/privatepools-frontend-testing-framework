07d9c445e60c68b6772fe238876801ff68c5c638
IvanKomaD3 <ivankoma@zohomail.eu>
2024-07-12 21:52:42 +0300

    feat: last changes

--- src/UI/Step.vue
+++ src/UI/Step.vue
@@ -3,13 +3,13 @@
     <div class="position-relative w-[35px] h-[35px]">
       <div
         v-if="activeStep < displayedActiveStep && !mmActive"
-        :class=" 'step_number'"
+        :class="'step_number'"
       >
-        {{  displayedActiveStep  }}
+        {{ displayedActiveStep }}
       </div>
       <div
         v-else-if="mmActive && activeStep === displayedActiveStep"
-        class="step_number "
+        class="step_number"
       >
         <img :src="metamask" width="20" />
       </div>
@@ -17,19 +17,28 @@
         v-else-if="activeStep > displayedActiveStep"
         class="step_number !bg-[#00E0FF]"
       >
-      <svg width="17" height="13" viewBox="0 0 17 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M5.57825 9.642L15.2202 0L16.5005 1.28025L5.57825 12.2025L0.5 7.12575L1.78025 5.8455L5.57825 9.642Z" fill="#02031C"/>
-</svg>
-
+        <svg
+          width="17"
+          height="13"
+          viewBox="0 0 17 13"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M5.57825 9.642L15.2202 0L16.5005 1.28025L5.57825 12.2025L0.5 7.12575L1.78025 5.8455L5.57825 9.642Z"
+            fill="#02031C"
+          />
+        </svg>
       </div>
-      <div
-        v-else
-        class="step_number "
-      >
-      {{  displayedActiveStep  }}
+      <div v-else class="step_number">
+        {{ displayedActiveStep }}
       </div>
     </div>
-    <div class="text-[12px] text-center w-[40px] text-[#00E0FF] font-['Syne',_sans-serif]">{{ stepText }}</div>
+    <div
+      class="text-[12px] text-center w-[40px] text-[#00E0FF] font-['Syne',_sans-serif]"
+    >
+      {{ stepText }}
+    </div>
   </div>
 </template>
 
--- src/components/Header/TokenDropdown.vue
+++ src/components/Header/TokenDropdown.vue
@@ -1,53 +1,50 @@
 <template>
   <Dropdown :distance="4" :placement="'bottom-center'">
-        <div class="button_drop dark:!bg-[#02031C] bg-[#DCEEF6] text-black dark:!text-white" style="padding: 10px">
-          <img :src="walletPoolsImg" />
+    <div
+      class="button_drop dark:!bg-[#02031C] bg-[#DCEEF6] text-black dark:!text-white"
+      style="padding: 10px"
+    >
+      <img :src="walletPoolsImg" />
 
-          $1.23
+      $1.23
+    </div>
+    <template #popper>
+      <div class="header__popup bg-white dark:!bg-[#02031C]">
+        <h4 class="text-[14px] text-black dark:!text-[#626262] mb-[10px]">
+          PPN {{ $t('token') }}
+        </h4>
+        <div
+          class="rewards_inside bg-[white] dark:!bg-[#22222224] text-black dark:!text-white"
+        >
+          <div class="text-[12px] text-black dark:!text-[#626262]">
+            PPN {{ $t('price') }}
+          </div>
+          <div class="rewards_numbers">$1.2345</div>
         </div>
-        <template #popper>
-            <div class="header__popup bg-white dark:!bg-[#02031C]">
-              <h4 
-                class="text-[14px] text-black dark:!text-[#626262] mb-[10px]"
-                >
-                PPN {{ $t('token') }}
-              </h4>
-              <div class="rewards_inside bg-[white] dark:!bg-[#22222224] text-black dark:!text-white">
-                <div  class="text-[12px] text-black dark:!text-[#626262]">
-                  PPN {{ $t('price') }}
-                </div>
-                <div class="rewards_numbers">
-                  $1.2345
-             
-                </div>
-              </div>
-              <div class="rewards_inside bg-[white] dark:!bg-[#22222224] text-black dark:!text-white">
-                <div class="text-[12px] text-black dark:!text-[#626262]">
-                  {{ $t('market_cap') }}
-                </div>
-                <div class="rewards_numbers">
-                  $1,827,643
-                </div>
-                
-              </div>
-              <div class="rewards_inside bg-[white] dark:!bg-[#22222224]  text-black dark:!text-white">
-                <div class="text-[12px] text-black dark:!text-[#626262]">
-                  {{ $t('circulating_supply') }}
-                </div>
-                <div class="rewards_numbers">
-                  6,001,859 PPN
-                </div>
-                
-              </div>
-              <div class="rewards_button_dropdown">{{ $t('add_ppn_to_wallet') }}</div>
-            </div>
-          </template>
-      </Dropdown>
+        <div
+          class="rewards_inside bg-[white] dark:!bg-[#22222224] text-black dark:!text-white"
+        >
+          <div class="text-[12px] text-black dark:!text-[#626262]">
+            {{ $t('market_cap') }}
+          </div>
+          <div class="rewards_numbers">$1,827,643</div>
+        </div>
+        <div
+          class="rewards_inside bg-[white] dark:!bg-[#22222224] text-black dark:!text-white"
+        >
+          <div class="text-[12px] text-black dark:!text-[#626262]">
+            {{ $t('circulating_supply') }}
+          </div>
+          <div class="rewards_numbers">6,001,859 PPN</div>
+        </div>
+        <div class="rewards_button_dropdown">{{ $t('add_ppn_to_wallet') }}</div>
+      </div>
+    </template>
+  </Dropdown>
 </template>
 <script setup>
 import { Dropdown } from 'floating-vue'
 import walletPoolsImg from '@/assets/icons/sidebarIcons/walletPoolsImage.svg'
-
 </script>
 <style lang="scss" scoped>
 .rewards_inside {
@@ -60,13 +57,12 @@ import walletPoolsImg from '@/assets/icons/sidebarIcons/walletPoolsImage.svg'
   line-height: 24px;
   // color: white;
   margin-bottom: 6px;
-
 }
 
 .rewards_button_dropdown {
   display: flex;
   align-items: center;
-  
+
   justify-content: center;
   cursor: pointer;
   margin-top: 16px;
@@ -77,22 +73,19 @@ import walletPoolsImg from '@/assets/icons/sidebarIcons/walletPoolsImage.svg'
   color: #000000;
   text-align: center;
   padding: 8px;
-  background: #00E0FF;
+  background: #00e0ff;
 
   &:hover {
-        filter: drop-shadow(0 0 0.3rem #00E0FF);
-
+    filter: drop-shadow(0 0 0.3rem #00e0ff);
   }
-
 }
-
 
 .button_drop {
   height: 40px;
- 
+
   font-family: 'Roboto Mono', monospace;
 
-  border: 1px solid #2ABDFF;
+  border: 1px solid #2abdff;
   // box-shadow: 0px 4px 4px 0px #00000040;
 
   border-radius: 100px;
@@ -116,6 +109,5 @@ import walletPoolsImg from '@/assets/icons/sidebarIcons/walletPoolsImage.svg'
 }
 .rewards_numbers {
   font-family: 'Roboto Mono', monospace;
-
 }
 </style>
