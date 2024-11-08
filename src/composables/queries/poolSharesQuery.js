b880c81dd917640496a78c0faf8acb1009dec637
SomeCEOCalledBernardo <94231329+bpssmarques@users.noreply.github.com>
2024-11-08 15:17:02 +0000

    update withdraw fix

--- src/composables/queries/poolSharesQuery.js
+++ src/composables/queries/poolSharesQuery.js
@@ -2,6 +2,7 @@ export function PoolSharesQuery(account, poolId) {
   return `query { poolShares (first: 300, where: {balance_gt: 0, userAddress: "${account.toLowerCase()}", poolId:"${poolId}"}) { poolId { id } balance percentage } }`
 }
 
+
 export function AllPoolSharesQuery(account) {
   return `query { poolShares (first: 300, where: {balance_gt: 0, userAddress: "${account.toLowerCase()}"}) { poolId { id } balance percentage } }`
 }
