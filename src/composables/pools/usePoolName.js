702c59483ec15523ca08525c389d585af5685183
typicalHuman <ruslanzhezh@zohomail.eu>
2024-11-15 13:03:09 +0500

    Update usePoolName.js

--- src/composables/pools/usePoolName.js
+++ src/composables/pools/usePoolName.js
@@ -1,15 +1,25 @@
 import { BACKEND_URL, REDUNDANT_BACKEND_URL } from '../pools/mappings'
 import axios from 'axios'
+import { InitializeMetamask } from '@/lib/utils/metamask'
 
 export async function addPoolName(networkId, poolId, owner, name) {
   let url = `${BACKEND_URL[networkId]}/pools/name`
   let response = null
+  const provider = await InitializeMetamask()
+  const signer = provider.getSigner()
+  let signature = null
+  try {
+    signature = await signer.signMessage(`Pool name: ${name}`)
+  } catch (e) {
+    console.error('User denied signature')
+    return false
+  }
   try {
     response = await axios.post(url, {
       poolAddress: poolId.slice(0, 42),
       owner,
       name,
-      signature: 'nosig',
+      signature,
     })
   } catch (e) {
     console.error('[POOL NAME ERROR]', e)
@@ -18,7 +28,7 @@ export async function addPoolName(networkId, poolId, owner, name) {
       poolAddress: poolId.slice(0, 42),
       owner,
       name,
-      signature: 'nosig',
+      signature,
     })
   }
   if (!response || !response.success) {
